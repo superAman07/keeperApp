c6caf993208c96ad99e0a15d15dbeae8f1aacc53
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-02-22 01:32:48 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AutorenewIcon from "@material-ui/icons/Autorenew";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AutorenewIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
